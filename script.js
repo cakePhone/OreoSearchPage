76e15ea1497f1f9cfb8349bd28b2423a8176eca7
cakePhone <ms8042939@gmail.com>
2023-02-08 18:26:15 +0000

    some shenanigans

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function search() {
     var input_value = document.getElementById("search-input").value
     console.log(input_value)
     if (/^\s*$/.test(input_value)) return
-    window.location.assign("https://duckduckgo.com/?q=" + encodeURIComponent(input_value))
+    if (input_value.startsWith("https://") || input_value.startsWith("http://")) {window.location.assign(encodeURIComponent(input_value))}
+    else {window.location.assign("https://duckduckgo.com/?q=" + encodeURIComponent(input_value))}
 }
 
 document.addEventListener("keypress", function(event) {
