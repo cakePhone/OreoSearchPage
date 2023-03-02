a33b0bc3b32f8df04bcddf2c8a34365181e729df
cakePhone <ms8042939@gmail.com>
2023-03-02 15:50:32 +0000

    big update

--- main.js
+++ main.js
@@ -9,10 +9,16 @@ const app = Vue.createApp({
         {"name": "Ecosia", "url": "https://www.ecosia.org/search?method=index&q="}
       ],
       inputNickname: (localStorage.getItem("username")) ? localStorage.getItem("username") : "",
+      accentColor: "#ffffff",
       searchInput: "",
-      accentColor: "",
       settingsClosed: true
     }
+  },
+  mounted() {
+    window.addEventListener("load", () => {
+      this.updateAccentColor()
+      document.querySelector("#accent-input").addEventListener("change", () => { this.updateAccentColor() })
+    })
   },
   methods: {
     greetings(username) {
@@ -39,10 +45,33 @@ const app = Vue.createApp({
     },
 
     updateLocalStorage(key, value) {
-      if(localStorage.getItem(key) === value) { return localStorage.getItem(key) }
-      localStorage.setItem(key, value)
-      console.log(`${key} was changed`)
+      //if(localStorage.getItem(key) === value) { return localStorage.getItem(key) }
+      if(value || value === "") { localStorage.setItem(key, value) }
+      // console.log(`${key} was changed`)
       return localStorage.getItem(key)
+    },
+
+    hexToRgb(hex) {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null
+    },
+
+    rgbToHex(r, g, b) {
+      return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
+    },
+
+    updateAccentColor() {
+      if(!localStorage.getItem("accentColor")) { localStorage.setItem("accentColor", "#ffffff") }
+      if(this.accentColor === "#ffffff") { this.accentColor = localStorage.getItem("accentColor") }
+      localStorage.setItem("accentColor", this.accentColor)
+
+      // console.log(this.hexToRgb(localStorage.getItem("accentColor")))
+
+      document.documentElement.style.setProperty("--accent-color", `${this.hexToRgb(this.accentColor).r},${this.hexToRgb(this.accentColor).g},${this.hexToRgb(this.accentColor).b}`)
     },
 
     search() {
@@ -77,15 +106,13 @@ const app = Vue.createApp({
     greeting() { return `${this.greetings(this.nickname)}` },
 
     searchEngine() {
-      if(!localStorage.getItem("searchEngine")) {localStorage.setItem("searchEngine", "0")}
-      return this.searchEngines[localStorage.getItem("searchEngine")]
+      if(!localStorage.getItem("searchEngine")) {this.updateLocalStorage("searchEngine", "0")}
+      return this.searchEngines[this.updateLocalStorage("searchEngine")]
     },
 
     nickname() {
       this.inputNickname = this.inputNickname.trim().length > 33 ? this.inputNickname.trim().slice(0, 30) + "..." : this.inputNickname
       return this.updateLocalStorage("username", this.inputNickname.trim())
     },
-
-
   }
 })
