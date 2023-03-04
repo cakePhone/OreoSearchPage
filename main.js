6d2e1562e0c6a87461388e4aab499c16f1519342
cakePhone <ms8042939@gmail.com>
2023-03-04 10:40:16 +0000

    Organised and commented main.js

--- main.js
+++ main.js
@@ -35,6 +35,23 @@ const app = Vue.createApp({
 
   //! Methods
   methods: {
+
+    //! Helper Methods
+    hexToRgb(hex) {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null
+    },
+
+    rgbToHex(r, g, b) {
+      return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
+    },
+
+    //! Main Experience Methods
+
     greetings(username) {
       const timenow = new Date().getHours()
       let timeOfDayString
@@ -57,24 +74,40 @@ const app = Vue.createApp({
         return `${timeOfDayString}!`
       }
     },
+    
+    search() {
+      if (/^\s*$/.test(this.searchInput)) return
+      const isValidUrl = urlString=> {
+        var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+        '(\\#[-a-z\\d_]*)?$','i') // validate fragment locator
+        
+        return !!urlPattern.test(urlString)
+      }
+
+      if (isValidUrl(this.searchInput)) {
+        if (this.searchInput.startsWith("https://") || this.searchInput.startsWith("http://")) {
+          window.location.href = this.searchInput
+        } else {
+          window.location.href = encodeURI("https://" + this.searchInput)
+        }
+      } else { window.location.href = this.searchEngines[this.searchEngine].url + encodeURIComponent(this.searchInput) }
+    },
+    
+    //! All settings related methods
 
     updateNickname() {
       localStorage.setItem("username", this.inputNickname)
     },
 
-    hexToRgb(hex) {
-      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-      return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      } : null
+    updateSearchEngine() {
+      if(!localStorage.getItem("searchEngine")) { localStorage.setItem("searchEngine", "0") }
+      localStorage.setItem("searchEngine", this.searchEngine)
     },
-
-    rgbToHex(r, g, b) {
-      return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
-    },
-
+    
     updateAccentColor() {
       if(!localStorage.getItem("accentColor")) { localStorage.setItem("accentColor", "#ffffff") }
       localStorage.setItem("accentColor", this.accentColor)
@@ -87,34 +120,6 @@ const app = Vue.createApp({
       localStorage.setItem("backgroundColor", this.backgroundColor)
 
       document.documentElement.style.setProperty("--background-color", `${this.hexToRgb(this.backgroundColor).r},${this.hexToRgb(this.backgroundColor).g},${this.hexToRgb(this.backgroundColor).b}`)
-
-    },
-
-    search() {
-      if (/^\s*$/.test(this.searchInput)) return
-      const isValidUrl = urlString=> {
-        var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-        '(\\#[-a-z\\d_]*)?$','i') // validate fragment locator
-
-        return !!urlPattern.test(urlString)
-      }
-
-      if (isValidUrl(this.searchInput)) {
-        if (this.searchInput.startsWith("https://") || this.searchInput.startsWith("http://")) {
-          window.location.href = this.searchInput
-        } else {
-          window.location.href = encodeURI("https://" + this.searchInput)
-        }
-      } else { window.location.href = this.searchEngines[this.searchEngine].url + encodeURIComponent(this.searchInput) }
-    },
-
-    updateSearchEngine() {
-      if(!localStorage.getItem("searchEngine")) { localStorage.setItem("searchEngine", "0") }
-      localStorage.setItem("searchEngine", this.searchEngine)
     },
 
     updateBackgroundImage(event) {
